e71a2a50cc936244646ea4a50465160b7bc4611c
asopaipa <eslounicoquese@gmail.com>
2025-02-25 20:33:45 +0100

    Update run.sh

--- acestream-engine/run.sh
+++ acestream-engine/run.sh
@@ -31,4 +31,4 @@ bashio::log.info "Ejecutando main.py con los siguientes parámetros:"
 bashio::log.info "python main.py $COMMAND_ARGS"
 
 # Ejecutar el comando
-exec /acestream/python /acestream/main.py $COMMAND_ARGS
+exec python main.py --client-console --live-cache-type memory --live-mem-cache-size 104857600 --disable-sentry --log-stdout $COMMAND_ARGS
