5197716ff1bd1ed88f1b328b469de8542ff6b163
asopaipa <eslounicoquese@gmail.com>
2025-02-26 19:50:52 +0100

    Update run.sh

--- acestream-engine/run.sh
+++ acestream-engine/run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 # Ejecutar el comando
 cd /acestream
 exec /acestream/python/bin/python ./main.py --client-console --live-cache-type memory --live-mem-cache-size 104857600 --disable-sentry --log-stdout $COMMAND_ARGS
